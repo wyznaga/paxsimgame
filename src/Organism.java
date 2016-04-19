88058b28e2b0fed827bab8a37f24ba797ac7247a
William Yznaga <wyznaga@gmail.com>
2016-04-19 22:16:34 +0000

    Added Organism superclass, subclassed to Prey, added WorldGrid class as 2D grid of Organism instances

---
+++ src/Organism.java
@@ -0,0 +1,21 @@
+import java.awt.Point;
+
+public class Organism {
+    public String name;
+    public Point location = new Point(); // construct new location Point at default of (0,0)
+    public enum State {
+        MOVE, LIVE, REPRODUCE, TIRE, EAT
+    }
+    public enum Movement {
+        NORTH, SOUTH, EAST, WEST, STATIC;
+    }
+
+    State currentState;
+    Movement currentMovement;
+    void simulate()
+    {
+        this.currentState = LIVE;
+        this.currentMovement = STATIC;
+    }
+}
+
