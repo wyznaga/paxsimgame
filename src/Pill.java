b8a898a0d94404266fc5506435874d11c5fb3977
William Yznaga <wyznaga@gmail.com>
2016-04-26 21:55:59 +0000

    Deleted Predator class finaly; edited others

---
+++ src/Pill.java
@@ -0,0 +1,11 @@
+public class Pill {
+    public int spawnRate;
+    public int getSpawnRate()
+    {
+        return this.spawnRate;
+    }
+    public void setSpawnRate(int rate)
+    {
+        this.spawnRate = rate;
+    }
+}
